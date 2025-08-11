23d53330248763b7e1c70465073ca98f67c39613
DummyDW-development <lifesonlyalien1@gmail.com>
2025-08-10 22:23:13 -0400

    Update scriptV0.0.2.js
    replaced images

--- scriptV0.0.2.js
+++ scriptV0.0.2.js
@@ -106,58 +106,58 @@ const characters = [
 
     {
         name: 'NlSickle',
-        img: 'https://pouch.jumpshare.com/preview/qJBRRYqbcLN2P4uAxYhHGFD8KZ6a6oVhjmQbl5nRkc5uQXm-bDIeGqQXvWfHGPIukQ4U8w6sdH2viZtsiJY1q120agdj49wH4JUUZhvY0RY'
+        img:'img/Nlsickle.svg'
         , placement: 'char'
 
     },
     {
         name: 'NlBeary',
-        img: 'https://pouch.jumpshare.com/preview/86YMwvxf-sK8HyfIvnZyN0k5C10ek3N_L0oX8hM3T-wHlcqMUrfHivElWH--mozrkQ4U8w6sdH2viZtsiJY1q5oM1bAdisCIaCwFYAl7yS0'
+        img:'img/Nlbeary.svg'
         , placement: 'char'
     },
     {
         name: '☆ S M I L E ☆',
-        img: 'https://pouch.jumpshare.com/preview/RFyR0b28qw71d7wwYeD504v-e9XMYRDvCr_4Y2Ld_4AjMnwVT1kYKW8wq4wVFYsqkQ4U8w6sdH2viZtsiJY1q_hDblGRc1He6voC44GHZBU'
+        img: 'img/☆ S M I L E ☆.svg'
         , placement: 'char'
     },
     {
         name: 'Cosmic Xin Error',
-        img: 'https://pouch.jumpshare.com/preview/NNmx1O-ykAJnrf-I5XHU2DWtNBx5c9095dXmcNteNScWlpg4JEBKN8EiYPsjUljskQ4U8w6sdH2viZtsiJY1q162TwoMTrSJ8gS3wD8aDEc'
+        img:'img/Cosmic.svg'
         , placement: 'char'
     },
     {
         name: 'Silly Goober',
-        img: 'https://pouch.jumpshare.com/preview/G1JHIeXdVF65HLjZ8qeqzoTa_rTLedIUiPZHVYHenJPMIEQa_oqRsA23Vd269Ws4kQ4U8w6sdH2viZtsiJY1q9OklsPjocx-KlWtRH6UO8Y'
+        img:'img/Silly Goober.svg'
         , placement: 'char'
     },
     {
         name: 'Cosmia//Leah',
-        img: 'https://pouch.jumpshare.com/preview/rPccTP1Yw3GAs-4ArTAj-5Ou4b2KoprpaPmOpJKtWtHEzJtUssUY9Doy8g6TAzQEkQ4U8w6sdH2viZtsiJY1qw8PmFY1ehPqJEn5qJ7kza0'
+        img:'img/Cosmia-Leah.svg'
         , placement: 'char'
     },
     {
         name: 'SashaRose',
-        img: 'https://pouch.jumpshare.com/preview/3B5hMVx79-FsKE3oXIDNNa8nxV1l4WUTJkSCoa0luGMsnxU9a0YmwdYqj0zmYWEnkQ4U8w6sdH2viZtsiJY1qw8PmFY1ehPqJEn5qJ7kza0'
+        img:'img/SashaRose.svg'
         , placement: 'char'
     },
     {
         name: 'Asher Dasher',
-        img: 'https://pouch.jumpshare.com/preview/32ZGBr6uIUtqIau28DvI_a7-_JOq8tLq0hcp_kXLItLGPyn4bD-GqquW-yP-JWsekQ4U8w6sdH2viZtsiJY1qw8PmFY1ehPqJEn5qJ7kza0'
+        img:'img/Asher.svg'
         , placement: 'char'
     },
     {
         name: 'Hauntuh/Emoji Man',
-        img: 'https://pouch.jumpshare.com/preview/bZ79fvh77_4MFCVDS4QmrFIi_isSSeG4tmNF1gJ6S_Y_m3mcuUpya4Z9VYSL6QjnkQ4U8w6sdH2viZtsiJY1qw8PmFY1ehPqJEn5qJ7kza0'
+        img:'img/Hauntuh.svg'
         , placement: 'char'
     },
     {
         name: 'Evadare/Kontos',
-        img: 'https://pouch.jumpshare.com/preview/xdBPAMXmfBTfAg8SxbAMKwp3evMFPfH6XCiT_mB3yJ200rJuoXUnVU4jcwFZhm5YkQ4U8w6sdH2viZtsiJY1qw8PmFY1ehPqJEn5qJ7kza0'
+        img:'img/Evadare.svg'
         , placement: 'char'
     },
     {
         name: 'Wanderer Orcaz',
-        img: 'https://pouch.jumpshare.com/preview/cY64qGTsQZCr_UHcmBL5SPAYYYkOM-73ig5CZn0l-PD8nYnpcP3BGp9H47mH-gNUkQ4U8w6sdH2viZtsiJY1qw8PmFY1ehPqJEn5qJ7kza0'
+        img:'img/Wanderer.svg'
         , placement: 'char'
     }
 ];
